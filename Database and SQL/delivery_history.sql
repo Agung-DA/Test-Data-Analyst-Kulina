af0516a2e965196ed1f490845db0968c9c52677a
Agung-DA <agungalbanjari21@gmail.com>
2025-12-10 13:31:38 +0700

    Update delivery_history.sql

--- Database and SQL/delivery_history.sql
+++ Database and SQL/delivery_history.sql
@@ -1,1 +1,26 @@
+with successful_deliveries AS (
+select u.id AS user_id, u.name AS user_name, u.email, u.phone, od.delivery_date, p.name AS product_name 
+c.name AS category_name, od.quantity, ul.address
+from ku_order_detail od
+join ku_order_detail_status ods on od.status = ods.id
+join ku_order o on od.order_id = o.id
+join ku_user u on o.user_id = u.id
+join ku_product p on od.product_id = p.id
+left join ku_product_category pc on p.id = pc.product_id
+left join ku_category c on pc.category_id = c.id
+left join ku_user_location ul on od.user_location_id = ul.id
+where ods.name = 'success' and od.delivery_date BETWEEN DATE '2025-09-01 00:00:00' AND DATE '2025-10-01 00:00:00'
+),
+  
+aggregated_category AS (
+select user_id, user_name, email, phone, delivery_date, product_name, STRING_AGG(category_name, ', ') AS product_categories,
+quantity, address
+from successful_deliveries
+group by 1,2,3,4,5,6,7,8
+)
 
+select *, sum(quantity) 
+over (
+partition by user_id order by delivery_date ) as total_quantity
+from aggregated_category
+order by delivery_date, user_id;

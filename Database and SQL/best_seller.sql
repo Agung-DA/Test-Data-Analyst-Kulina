c7d0a96e2dfd932fcbb21a0b012f4cc4a4ee74f9
Agung-DA <agungalbanjari21@gmail.com>
2025-12-10 13:43:53 +0700

    Update best_seller.sql

--- Database and SQL/best_seller.sql
+++ Database and SQL/best_seller.sql
@@ -1,1 +1,13 @@
-
+with successful_orders as (
+select p.id product_id, p.name product_name, u.id user_id, p.price, od.quantity
+from ku_order_detail od
+join ku_order_detail_status ods on od.status = ods.id
+join ku_order o on od.order_id = o.id
+join ku_user u on o.user_id = u.id
+join ku_product p on od.product_id = p.id
+where ods.name = 'success' and od.delivery_date between DATE '2025-07-01 00:00:00' and DATE '2025-10-01 00:00:00'
+)
+select product_name, sum(price * quantity) total_gmv, sum(quantity) total_quantity, count(distinct user_id) user_count
+from successful_orders
+group by product_name
+order by total_gmv desc, total_quantity desc;
